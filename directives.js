923672d833f76d2fbb1ac23a636b4c6de194ebc3
Francisco Ruiz <quiesagua@gmail.com>
2014-09-07 19:03:16 +0100

    Changed menu directive to set max-height instead of height for the panel

--- directives.js
+++ directives.js
@@ -5,7 +5,7 @@ directives.directive('menu', function ($window) {
 	var link = function (scope, element, attrs) {
 		var windowHeight = $window.innerHeight;
 		console.log('Setting menu height to %spx', windowHeight)
-		element.css('height', windowHeight + 'px');
+		element.css('max-height', windowHeight + 'px');
 		console.log(element, element.css('height'));
 	};
 
