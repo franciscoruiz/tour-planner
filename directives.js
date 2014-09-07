e2cd58d50530a4d0c7c3aec84a5ba43456c07214
Francisco Ruiz <quiesagua@gmail.com>
2014-09-07 21:52:25 +0100

    Improved debugging info logged by the menu directive

--- directives.js
+++ directives.js
@@ -1,13 +1,12 @@
 var directives = angular.module('planner.directives', []);
 
 
-directives.directive('menu', function ($window) {
-	var link = function (scope, element, attrs) {
-		var windowHeight = $window.innerHeight;
-		console.log('Setting menu height to %spx', windowHeight)
-		element.css('max-height', windowHeight + 'px');
-		console.log(element, element.css('height'));
-	};
+directives.directive('menu', function ($window, $log) {
+  var link = function (scope, element, attrs) {
+    var windowHeight = $window.innerHeight;
+    element.css('max-height', windowHeight + 'px');
+    $log.debug('Limited menu height to %spx', windowHeight);
+  };
 
-	return {link: link};
+  return {link: link};
 });
